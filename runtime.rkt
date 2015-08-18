cdd8c25e56f892c2e46067cf1cd24c75f394971f
Jens Axel Søgaard <jensaxel@soegaard.net>
2015-08-18 19:22:54 +0200

    Add reverse. Use reverse in for/list.

---
+++ runtime.rkt
@@ -0,0 +1,165 @@
+#lang racket
+
+;; TODO Fix the bug below. The second s has not been alpha-renamed.
+
+;> function string__glist(s){
+;    return ((function(xs){{
+;              var s=s,n=s.length,i=0;
+;              while((true&&true&&(i<n))){
+;                ((function(c){
+;                  (xs=(cons(c,xs)));
+;                  return (i+=1);})(s[i]));
+;               };undefined;
+;             }
+;            return xs;
+;         })(NULL));};
+
+(require "urlang.rkt" "for.rkt" syntax/parse)
+
+(current-urlang-run?                           #t)
+(current-urlang-echo?                          #t)
+(current-urlang-console.log-module-level-expr? #t)
+
+;;; Add  in-list  to for and friends
+(define (handle-in-list clause)
+  (syntax-parse clause
+    #:literals (in-list)
+    [[x in-list list-expr]
+     #'(([xs list-expr])        ; list of var clauses to create initial state
+        (not (null? xs))        ; termination condition
+        ([x (car xs)])          ; let bindings (needs to bind x)
+        ((:= xs (cdr xs))))]))  ; statements to step state forward
+
+(define (handle-in-vector clause)
+  (syntax-parse clause
+    #:literals (in-vector)
+    [[x in-vector vector-expr]
+     #'(([xs vector-expr]
+         [n (vector-length xs)]
+         [i 0])                  ; list of var clauses to create initial state
+        (< i n)                  ; termination condition
+        ([x (ref xs (+ i 1))])   ; let bindings (needs to bind x)
+        ((+= i 1)))]))
+
+(add-clause-handler! 'in-list   handle-in-list)
+(add-clause-handler! 'in-vector handle-in-vector)
+
+;;; for/list
+
+(define (expand-for/list stx)
+  (syntax-parse stx
+    [(_for/list (clause ...) statement-or-break ... expr)
+     (syntax/loc stx
+       (let ([xs NULL])
+         (for (clause ...)
+           statement-or-break ...
+           (:= xs (cons expr xs)))
+         (reverse xs)))]))  ; note: use reverse! ?
+
+(define-urlang-macro for/list expand-for/list)
+
+
+(display
+ (urlang
+  (urmodule runtime
+    (export null? pair? list? cons car cdr)
+    (import Array Number String typeof)
+    ;;; Array
+    (define (array? v) ; todo : inline array?
+      (Array.isArray v))
+    ;;; Tags
+    (define (tag a) (ref a 0))
+    (define PAIR   (array "pair"))
+    (define VECTOR (array "vector"))
+    (define CHAR   (array "char"))
+    ;; Empty
+    (define NULL (array))
+    (define (null? v) (= v NULL))
+    ;;; Pairs
+    (define (pair? v)      (and (array? v) (= (tag v) PAIR)))
+    (define (list? v)      (or (= v NULL) (and (array? v) (= (tag v) PAIR) (ref v 1))))
+    (define (cons a d)     (array PAIR (list? d) a d))
+    (define (unsafe-car p) (ref p 2))
+    (define (unsafe-cdr p) (ref p 3))
+    (define car unsafe-car)
+    (define cdr unsafe-cdr)
+    ;;; Vectors
+    (define (vector? v)       (and (array? v) (= (tag v) VECTOR)))
+    (define (vector-length v) (- v.length 1)) ; disregard tag
+    (define (vector-ref v i)  (ref v (+ i 1)))      
+    (define (vector) ; multiple arguments
+      (var [args arguments] [n args.length] [a (Array (+ n 1))])
+      (array! a 0 VECTOR)
+      (for ([j in-range 0 n])
+        (array! a (+ j 1) (ref args j)))
+      a)
+        
+    ;;; Lists
+    (define (length xs)
+      (var (n 0))
+      (while (pair? xs) (+= n 1) (:= xs (cdr xs)))
+      n)
+    (define (reverse xs)
+      ; Note: for/list uses reverse, so reverse can't use for/list
+      (var [n (length xs)]
+           [a (Array n)]
+           [i 0]
+           [t xs])
+      (while (not (null? t))
+             (array! a i (car t))
+             (+= i 1)
+             (:= t (cdr t)))
+      (var [rs NULL])
+      (:= i 0)
+      (while (< i n)
+             (:= rs (cons (ref a i) rs))
+             (+= i 1))
+      rs)
+    ;;; Chars
+    (define (make-char js-string) (array CHAR js-string))
+    (define (char->string c)      (ref c 1))
+    (define (char->integer c)
+      (var [s (ref c 1)])
+      (s.charCodeAt 0))
+      
+    ;;; Strings
+    (define (string? v)           (= (typeof v) "string"))
+    (define (string-ref s i)      (make-char (ref s i)))
+    (define (string-append s t)   (+ s t))
+    (define (string->list s)      (for/list ([c in-string s]) c))
+    ;;; Numbers
+    (define (number? v) (= (typeof v) "number"))
+    
+    ;;;
+    ;;; Higher Order
+    ;;;
+
+    ;;; FORMAT
+    ;; The str functions convert values into strings.
+    (define (str-null)      "()")
+    (define (str-number v)  (v.toString))
+    (define (str-string v)  (+ "\\\"" v "\\\""))
+    (define (str-list v)
+      (var [a (for/array ([x in-list v]) (str x))])
+      (+ "(" (a.join " ") ")"))
+    (define (str-vector v)
+      (var [a (for/array ([x in-vector v]) (str x))])
+      (+ "#(" (a.join " ") ")"))
+    (define (str-pair v)   (+ "(" (str (car v)) " . " (str (cdr v)) ")"))
+    (define (str v)
+      (if (null? v) (str-null)
+          (if (string? v) (str-string v)
+              (if (number? v) (str-number v)
+                  (if (list? v) (str-list v)
+                      (if (pair? v) (str-pair v)
+                          (if (vector? v) (str-vector v)
+                              "str - internal error")))))))
+      
+    ; (str (cons 10 (cons 11 (cons 12 NULL))))
+    ; (str (vector 10 11 12))
+    ; (str (string-append "foo" "bar"))
+    (str-list (string->list "foobar"))
+    (str-list (for/list ([x in-range 0 5]) x))
+    (str-list (reverse (for/list ([x in-range 0 5]) x)))
+    (str-list (reverse (string->list "foobar")))
+  )))
