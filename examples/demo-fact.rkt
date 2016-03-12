316338b3f44be63bcc611cfbe861550773329239
Jens Axel Søgaard <jensaxel@soegaard.net>
2016-03-12 23:55:08 +0100

    Added factorial example

---
+++ examples/demo-fact.rkt
@@ -0,0 +1,16 @@
+#lang racket
+(require urlang/urlang)
+
+(urlang
+ (urmodule demo-fact                                           ; module name
+   (export fact)                                          ; fact is exported
+   (define (fact n) (if (= n 0) 1 (* n (fact (- n 1)))))
+   (console.log (fact 5))))
+
+;;; Running the program will create a file "demo-fact.js" containing:
+
+;  "use strict";
+;  function fact(n){return (((n===0)===false)?(n*(fact((n-1)))):1);};
+;  (console.log((fact(5))));
+;  exports.fact=fact;
+
