f10771843ac11c640b5bbd333cb4081e68232887
Jens Axel Søgaard <jensaxel@soegaard.net>
2016-03-13 20:40:07 +0100

    Add info.rkt to urlang

---
+++ urlang/info.rkt
@@ -0,0 +1,18 @@
+#lang info
+;;; This file describes the contents of "urlang" collection in the "urlang" package.
+
+;;; The name of the collections is simply "urlang".
+(define collection "urlang")
+
+;;; Version number
+(define version    "1.0")
+
+;;; Dependencies declared here will need source
+(define deps       '())
+
+;;; Dependencies here can be installed as binaries (i.e. zo-files)
+(define build-deps '("base"
+                     "rackunit-lib"))
+
+(define compile-omit-paths '())
+
