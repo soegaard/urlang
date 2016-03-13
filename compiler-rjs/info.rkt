fbea42bf2dfe5bc2c83955dc125bf964f6a79a3e
Jens Axel Søgaard <jensaxel@soegaard.net>
2016-03-13 20:37:23 +0100

    Add info.rkt to compiler-rjs collection

---
+++ compiler-rjs/info.rkt
@@ -0,0 +1,23 @@
+#lang info
+;;; This file describes the contents of the Urlang package.
+;;; The information is used by the Racket package system.
+
+;;; The Urlang package contains multiple collections (urlang, urlang-doc, compiler etc)
+(define collection "compiler-rjs")
+
+;;; Version number
+(define version    "1.0")
+
+;;; Dependencies declared here will need source
+(define deps       '())
+
+;;; Dependencies here can be installed as binaries (i.e. zo-files)
+(define build-deps '("base"
+                     "nanopass"                     
+                     "at-exp-lib"
+                     "rackunit-lib"
+                     "scribble-lib"
+                     "racket-doc"))
+
+(define compile-omit-paths '())
+
